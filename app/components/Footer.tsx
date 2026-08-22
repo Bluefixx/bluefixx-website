d1dcc92321edc70a9942c6c070e0c2c659cc2a0c
tomdevy <agboolaemmanuel524@gmail.com>
2026-08-22 21:20:35 +0100

    final touches

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -76,7 +76,7 @@ export default function Footer() {
           </div>
         </div>
 
-        {/* Links Columns Container */}
+        {/* Links Columns Container*/}
         <div className="grid grid-cols-2 lg:grid-cols-4 gap-8 lg:gap-16">
           {/* Platform */}
           <div className="flex flex-col gap-4">
